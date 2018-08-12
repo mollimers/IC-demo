b3bfe30d14c04beae8c62aebf40a9478efdd84b4
David Gillberg <david@mollimers.com>
2018-08-12 17:23:07 +0200

    Basic div creator using a predefined div that is modified at button click.

--- PimpMyRect/src/editor.js
+++ PimpMyRect/src/editor.js
@@ -10,24 +10,36 @@
  *              div_border_radius(id, radius)
  *              save_div(id)
  ***********************************************************************/
-function div_height(id, size) {
-    var size_string = size.toString() + "px";
+/*     var heightInput = document.querySelector('#height') as HTMLInputElement;
+    var widthInput = document.querySelector('#width') as HTMLInputElement;
+    var radiusInput = document.querySelector('#radius') as HTMLInputElement; */
+function div_height(id) {
+    var heightInput = document.querySelector('#height');
+    var size_string = heightInput.value.toString() + "px";
     document.getElementById(id).setAttribute("height", size_string);
     document.getElementById(id).style.height = size_string;
 }
-function div_width(id, size) {
-    var size_string = size.toString() + "px";
+function div_width(id) {
+    var widthInput = document.querySelector('#width');
+    var size_string = widthInput.value.toString() + "px";
     document.getElementById(id).setAttribute("width", size_string);
     document.getElementById(id).style.width = size_string;
 }
 function div_color(id, color) {
+    var radiusInput = document.querySelector('#radius');
     document.getElementById(id).setAttribute("background-color", color);
     document.getElementById(id).style.backgroundColor = color;
 }
-function div_border_radius(id, radius) {
-    var radius_string = radius.toString() + "px";
+function div_border_radius(id) {
+    var radiusInput = document.querySelector('#radius');
+    var radius_string = radiusInput.value.toString() + "px";
     document.getElementById(id).setAttribute("border-radius", radius_string);
     document.getElementById(id).style.borderRadius = radius_string;
 }
 function save_div(id) {
+    div_height("div_1"); //, 30);
+    div_width("div_1"); //, 30);
+    console.log("div W and H set");
+    div_color("div_1", "lightblue");
+    div_border_radius("div_1");
 }
