5e72f17f1b23ab82e901384e00fe49b7761046a7
David Gillberg <david@mollimers.com>
2018-08-12 06:38:13 +0200

    Update fibtest.ts
    uncomment the first block of tests

--- Fibonacci/test/fibtest.ts
+++ Fibonacci/test/fibtest.ts
@@ -1,7 +1,7 @@
 import { Fibonacci } from '../src/fibonacci';
 
 let fibbo = new Fibonacci();
-/* fibbo.test_encode(0);
+fibbo.test_encode(0);
 fibbo.test_encode(1);
 fibbo.test_encode(2);
 fibbo.test_encode(3);
@@ -11,7 +11,7 @@ fibbo.test_encode(6);
 fibbo.test_encode(7);
 fibbo.test_encode(8);
 fibbo.test_encode(9);
-fibbo.test_encode(10); */
+fibbo.test_encode(10);
 fibbo.test_decode('11');
 fibbo.test_decode('011');
 fibbo.test_decode('0011');
