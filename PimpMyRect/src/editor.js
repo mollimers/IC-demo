c4c49d90020e3ad937f4b5f918f78eb4a287d756
David Gillberg <david@mollimers.com>
2018-08-13 22:48:31 +0200

    Non-persistent gallery implemented. Multiple divs can be saved. Color picker live updates the demo div

--- PimpMyRect/src/editor.js
+++ PimpMyRect/src/editor.js
@@ -9,7 +9,10 @@
  *              div_color(id, color)
  *              div_border_radius(id, radius)
  *              save_div(id)
+ *              no_enter(evt)
  ***********************************************************************/
+// TODO: when we have persistance implemented, this variable should be stored and read there
+var divId = 0;
 function div_height(id) {
     var heightInput = document.querySelector('#height');
     var size_string = heightInput.value.toString() + "px";
@@ -45,13 +48,29 @@ function div_border_radius(id) {
     document.getElementById(id).setAttribute("border-radius", radius_string);
     document.getElementById(id).style.borderRadius = radius_string;
 }
-function save_div(id) {
-    div_height("demoDiv");
-    div_width("demoDiv");
-    div_color("demoDiv");
-    div_border_radius("demoDiv");
+function save_div() {
+    var prevDivId = "div_" + divId;
+    divId++; // increase the id variable
+    // create a new div to add to the gallery
+    var newDiv = document.createElement("div");
+    // give the div an id, so that we can reference it further on
+    var newDivId = newDiv.id = "div_" + divId;
+    console.log("newDiv: " + newDiv);
+    console.log("newDivId: " + newDivId);
+    // add the newly created element to the DOM
+    var previousDiv = document.querySelector('#div_0');
+    var galleryDiv = document.querySelector('.divGallery');
+    console.log("previousDiv: " + previousDiv);
+    console.log("prevDivId: " + prevDivId);
+    console.log("galleryDiv: " + galleryDiv);
+    galleryDiv.appendChild(newDiv);
+    //document.body.insertBefore(newDiv, previousDiv); 
+    div_height(newDivId);
+    div_width(newDivId);
+    div_color(newDivId);
+    div_border_radius(newDivId);
 }
-function noenter(evt) {
+function no_enter(evt) {
     if (evt.keyCode == 13) {
         return false;
     }
