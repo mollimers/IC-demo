71ef4049ab17c1b42269c09dd7cf3d0617d0e92c
David Gillberg <david@mollimers.com>
2018-08-16 08:54:59 +0200

    THIS is what was used for the demo.

--- PimpMyRect/test/test_mongodb.ts
+++ PimpMyRect/test/test_mongodb.ts
@@ -2,15 +2,17 @@ import * as mdb from '../src/mdb';
 let mdbHandler = new mdb.mdbHandler();
  
 //post div data to mongodb
-mdbHandler.post("div_3", "30px", "30px", "green", "30px");
-mdbHandler.post("div_2", "20px", "20px", "red", "20px");
-mdbHandler.post("div_1", "10px", "10px", "blue", "10px");
-mdbHandler.post("div_4", "40px", "40px", "orange", "40px");
+mdbHandler.post("30px", "30px", "green", "30px");
+mdbHandler.post("20px", "20px", "red", "20px");
+mdbHandler.post("10px", "10px", "blue", "10px");
+mdbHandler.post("40px", "40px", "orange", "40px");
+console.log("post");
 
 //get div data from mongodb
-mdbHandler.get_all();
+/* mdbHandler.get_all();
+console.log("get"); */ 
 
 //delete div data in mongodb
-mdbHandler.delete("div_2");
-mdbHandler.delete("div_3");
-mdbHandler.delete("div_1");
+/* mdbHandler.delete("orange");
+mdbHandler.delete("red");
+mdbHandler.delete("green"); */
